670fa41f0bd2e0b0cf83107230a591c79eabd9e6
ashish <ashish@riverus.in>
2018-06-21 12:18:49 +0530

    routing done

--- src/App/Components/app.tsx
+++ src/App/Components/app.tsx
@@ -1,20 +1,29 @@
 import * as React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Router } from 'react-router';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
 
 export interface Props {
-
 }
-export interface State {
 
-}
-export default class App extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-  }
-  render() {
-    return(
-      <div>
-        Hello Bitches!!
+export default class App extends React.Component<Props, {}> {
+    constructor(props: Props) {
+        super(props);
+    }
+    render() {
+ 
+    var RoutingApp = (
+    <div>
+      <Router history={history}>
+        <div>
+          <Switch>
+            <Route path="/"/>
+          </Switch>
         </div>
-    );
-  }
+      </Router>
+    </div>);
+    return RoutingApp;
+}
 }
